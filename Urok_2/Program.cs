f593a58ebbb65bd96703dc1a897c0130df8015ea
SergeyK <sav1575@mail.ru>
2023-09-04 16:22:25 +0300

    ошибка не ясна

--- Urok_2/Program.cs
+++ Urok_2/Program.cs
@@ -81,6 +81,15 @@ for (int i = -num; i < num; i++)
 /*Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 	456 -> 6
 	782 -> 2
-	918 -> 8*/
-    
-
+	918 -> 8*/    
+    Console.Write("Enter a namber: " );
+num = Covert.ToInt32(Console.ReadLine());
+if (num >99 && num < 1000)
+{ 
+    int newNum = num %10;
+    Console.WriteLine(newNum);
+}
+else
+{ 
+    Console.WriteLine("Incorrect number!");
+}
